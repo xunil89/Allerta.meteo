ca130cbbc26baf03becbbfdc4412bb8687fcc02d
xunil89 <manuelcamillo@gmail.com>
2017-08-12 16:18:56 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,25 +1,10 @@
 <?php
-$content = file_get_contents("php://input");
-$update = json_decode($content, true);
 
-if(!$update)
-{
-  exit;
-}
+// https://api.telegram.org/bot414753725:AAENDyA3eOdFzDPC9HeWn0hnRH9EDGkn8A
+$token = "414753725:AAENDyA3eOdFzDPC9HeWn0hnRH9EDGkn8A";
+$website = "https://api.telegram.org/bot".$token;
 
-$message = isset($update['message']) ? $update['message'] : "";
-$messageId = isset($message['message_id']) ? $message['message_id'] : "";
-$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
-$date = isset($message['date']) ? $message['date'] : "";
-$text = isset($message['text']) ? $message['text'] : "";
+$getme = file_get_contents($website."/getme");
 
-$text = trim($text);
-$text = strtolower($text);
-
-header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
-$parameters["method"] = "sendMessage";
-echo json_encode($parameters);
+print_r($getme);
+?>
