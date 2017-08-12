4bd8f8c6fcf86716269fba1789ab63c32796dd47
xunil89 <manuelcamillo@gmail.com>
2017-08-12 16:06:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php echo "Silence is golden";
+<?php
+
+// https://api.telegram.org/bot414753725:AAENDyA3eOdFzDPC9HeWn0hnRH9EDGkn8A
+$token = "414753725:AAENDyA3eOdFzDPC9HeWn0hnRH9EDGkn8A";
+$website = "https://api.telegram.org/bot".$token;
+
+$getme = file_get_contents($website."/getme");
+
+print_r($getme);
+?>
