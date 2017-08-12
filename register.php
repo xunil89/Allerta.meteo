e357daf43ed46599bd4aeec165fbcb1d1632d030
xunil89 <manuelcamillo@gmail.com>
2017-08-12 15:11:39 +0200

    Update register.php

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 
 // PARAMETRI DA MODIFICARE
 $WEBHOOK_URL = 'http://t.me/allerta_meteo_bot';
-$BOT_TOKEN = '414753725:AAE6dL7bHukU6UkbFyGkY722IAjwGM3dvkc';
+$BOT_TOKEN = '';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
